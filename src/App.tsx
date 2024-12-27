196e61dfe72d8e7c769de2a099934557d2975b0c
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-27 20:14:55 +0530

    ± App
    added Navbar , All Routes and Footer Components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <>
+      <Navbar />
+      <AllRoutes />
+      <Footer />
+    </>
   );
 }
 
