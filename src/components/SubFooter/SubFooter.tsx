c46895df8e68517e2bd64d0c6cd4355a7244ccd6
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-27 20:13:28 +0530

    Create SubFooter.tsx

---
+++ src/components/SubFooter/SubFooter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SubFooter: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-[42px] w-[100%] text-center text-[12px] border-t-[1px] border-zinc-100/50">
+      Designed with vision by Jitendra, brought to life by Priyanshu—crafted
+      with precision and a touch of <span className="animate-pulse">❤️</span>.
+    </div>
+  );
+};
+
+export default SubFooter;
