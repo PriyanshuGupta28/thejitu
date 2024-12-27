755630dc5f2bb3e4fc971e0cc8538e7d4d5c4edf
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-27 20:13:40 +0530

    Create GradientText.tsx

---
+++ src/components/GradientText.tsx/GradientText.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface GradientTextProps {
+  children: React.ReactNode;
+  className?: string; // Allows passing additional classes
+}
+
+const GradientText: React.FC<GradientTextProps> = ({ children, className }) => {
+  return (
+    <span
+      className={`bg-gradient-to-r from-[#E43D19] via-[#3472BC] to-[#06D7D7] bg-clip-text text-transparent ${className}`}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default GradientText;
