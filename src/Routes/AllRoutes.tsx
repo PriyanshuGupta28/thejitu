8c6349cb2596bf5c07fa29b028e6cef973a9a616
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-28 01:20:22 +0530

    Update AllRoutes.tsx

--- src/Routes/AllRoutes.tsx
+++ src/Routes/AllRoutes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import Home from "../pages/Home/Home";
+import Ios from "../pages/Ios/Ios";
 
 const AllRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/ios" element={<Ios />} />
     </Routes>
   );
 };
