dba887f3ab79bf10d445699e394e90e4a1ea2191
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-28 18:37:37 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        display: ['"SF Pro Display"', "sans-serif"],
+        text: ['"SF Pro Text"', "sans-serif"],
+        textThin: ['"SF Pro Text Thin"', "sans-serif"],
+        compact: ["SF Pro Compact", "sans-serif"],
+        rounded: ['"SF Pro Compact Rounded"', "sans-serif"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
