0e5abbf6731fa506e28a28ba7966a2e6b80c23c8
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-30 17:34:45 +0530

    Update ProjectsSection.tsx

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -4,15 +4,17 @@ import GifBackgroundCard from "../GifBackgroundCard/GifBackgroundCard";
 
 const ProjectsSection: React.FC = () => {
   return (
-    <div className="flex flex-col gap-[20px] py-[24px] w-[928px]">
+    <div className="flex flex-col gap-[20px] py-[24px] w-[100%] md:w-[928px]">
       <div className="flex flex-col items-center gap-[20px]">
         <img
           src="https://s3-alpha-sig.figma.com/img/fb53/b23d/fa08b9d0d398c8c13bb0af77f4505c84?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VEyDCd3DI11IytDvy4Ykt8N~2xMqTBPG8jTxT4b7CZJvMguJz39WNlc3rZgnRYtTDFQkMs6Hq6oertfHAQor4Qpcr1s8TkpMeaBymHdV7Ke0ZQFcbu76t3Colrm53EFPOWE5MZRTGKUuup5-VbXCdAQreU-DbOjhnOw8Emw15oKdHgBZVn75VeZIUs-0wiSmGfHvvYWZCOlqAfYXlR37EpZhjwg43JK2zhvTg2~-EeyvqmDmjapkqvqEXLUWtCTPxOfDM1Ek3K73~Wu~aXP0w3Av~4rkXCgp95mPJO8WCMOEVlfi9TZpF68gHEQQQx4uLBSJdlg2WCC0c8zlrUGW6Q__"
           alt="jituthumb_img"
           className="w-[79.65px] h-[90px]"
         />
-        <h2 className="text-[32px] font-semibold">Case studies and projects</h2>
-        <div className="flex gap-[20px]">
+        <h2 className="text-[24px] md:text-[32px] font-semibold">
+          Case studies and projects
+        </h2>
+        <div className="flex gap-[20px] flex-col md:flex-row">
           <GifBackgroundCard
             title="iOS Case Study"
             description={["UX Correction", "Case Study", "Self"]}
@@ -28,7 +30,7 @@ const ProjectsSection: React.FC = () => {
             icon={<Lock size={12} />}
           />
         </div>
-        <div className="flex gap-[20px]">
+        <div className="flex flex-col md:flex-row gap-[20px]">
           <GifBackgroundCard
             title="iOS Case Study"
             description={["UX Correction", "Case Study", "Self"]}
