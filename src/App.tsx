f02ea08a73aaed5cdb6a1be8c33fac874a0a2c14
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-30 17:34:08 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AnimatedBackground from "./components/AnimatedBackground/AnimatedBackground";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import AllRoutes from "./Routes/AllRoutes";
@@ -6,9 +7,19 @@ import AllRoutes from "./Routes/AllRoutes";
 function App() {
   return (
     <>
-      <Navbar />
-      <AllRoutes />
-      <Footer />
+      <AnimatedBackground />
+      <div
+        style={{
+          position: "relative",
+          zIndex: 100,
+          color: "white",
+          paddingTop: "72px",
+        }}
+      >
+        <Navbar />
+        <AllRoutes />
+        <Footer />
+      </div>
     </>
   );
 }
