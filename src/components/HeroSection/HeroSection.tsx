bdc05388eddd1717a8a95e0494e67c2bd5d6bb79
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-30 17:34:22 +0530

    Update HeroSection.tsx

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,25 +4,25 @@ import GlowingCard from "../GlowingCard/GlowingCard";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="flex py-[24px] flex-col md:flex-row justify-center gap-[16px]">
-      <div className="flex flex-col gap-[24px] p-[48px] border border-zinc-700 rounded-[48px] w-[613px] bg-gradient-to-b from-[#101010] to-[#000000] relative">
-        <h1 className="text-[32px]">
+    <div className="flex py-[24px] flex-col justify-center items-center">
+      <GlowingCard />
+      <div className="flex flex-col gap-[24px] p-[48px] justify-center items-center ">
+        <h1 className="text-[32px] text-center">
           <span>Designing</span> <GradientText>stories</GradientText>
           <span>, not just screens.</span>
         </h1>
-        <p className="text-[12px] font-normal">
+        <p className="text-[14px] font-normal text-center w-[100%] md:w-[710px] text-white/80">
           Hi, I’m Jitendra. I specialize in creating user-centric, visually
           engaging products that simplify complex workflows. My approach blends
           problem-solving, storytelling, and design thinking to deliver
           impactful solutions.
         </p>
-        <p className="text-[12px] font-normal">
+        <p className="text-[14px] font-normal text-center w-[100%] md:w-[710px] text-white/80">
           When I’m not designing, you’ll find me breaking down design patterns,
           writing UX insights, learning new languages, or pushing limits at the
           gym.
         </p>
       </div>
-      <GlowingCard />
     </div>
   );
 };
