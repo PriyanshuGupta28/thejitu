f0dd08bfdbb665ba8e0e2875fbe43e172f89d04e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-31 12:30:06 +0530

    Create Journey.tsx

---
+++ src/components/Hubilio/Journey.tsx
@@ -0,0 +1,214 @@
+import React from "react";
+
+const Journey: React.FC = () => {
+  return (
+    <div className="h-[498.28px] pb-12 flex-col justify-center items-start gap-[11.37px] inline-flex relative">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1024"
+        height="421"
+        viewBox="0 0 1024 421"
+        fill="none"
+        className="absolute stroke-[1.066px] h-[418.554px]"
+      >
+        <path
+          d="M0 180.925H98.8061C126.641 180.925 149.43 201.877 149.43 227.497V372.982C149.43 398.589 172.205 419.554 200.053 419.554H275.034C302.869 419.554 325.658 398.602 325.658 372.982V126.299C325.658 100.692 348.433 79.7274 376.281 79.7274H448.194C476.029 79.7274 498.817 100.68 498.817 126.299V294.932C498.817 320.539 521.593 341.504 549.441 341.504H623.44C651.275 341.504 674.064 320.552 674.064 294.932V47.5715C674.064 21.9647 696.839 1 724.687 1H795.618C823.466 1 846.241 21.9522 846.241 47.5715V155.895C846.241 181.502 869.016 202.467 896.865 202.467H1141.25"
+          stroke="url(#paint0_linear_2216_10195)"
+          stroke-width="1.06593"
+          stroke-miterlimit="10"
+          stroke-dasharray="3.55 3.55"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_2216_10195"
+            x1="1141.25"
+            y1="210.277"
+            x2="0"
+            y2="210.277"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#E43D19" />
+            <stop offset="0.5" stop-color="#3472BC" />
+            <stop offset="1" stop-color="#06D7D7" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <div className="w-[1141.25px] h-[450.28px] relative">
+        <div className="origin-top-left rotate-90 w-[28.42px] h-[2.57px] left-[77.12px] top-[139.28px] absolute"></div>
+        <div className="w-[34.11px] h-[34.11px] left-[58.27px] top-[165.57px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-[15px] absolute bg-[#224341] rounded-full" />
+          <div className="w-[17.05px] h-[17.05px] left-[8.53px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="w-[34.11px] h-[34.11px] left-[132.17px] top-[253.69px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute bg-[#26313e] rounded-full" />
+            <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[4.26px] absolute bg-[#26323f] rounded-full" />
+          </div>
+          <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="w-[34.11px] h-[34.11px] left-[481.80px] top-[210.34px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute bg-[#26394f] rounded-full" />
+            <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[4.26px] absolute bg-[#26394f] rounded-full" />
+          </div>
+          <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="w-[34.11px] h-[34.11px] left-[656.61px] top-[289.93px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute bg-[#402f49] rounded-full" />
+            <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[4.26px] absolute bg-[#402f49] rounded-full" />
+          </div>
+          <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="w-[34.11px] h-[34.11px] left-[656.61px] top-[90.25px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute bg-[#402f49] rounded-full" />
+            <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[4.26px] absolute bg-[#402f49] rounded-full" />
+          </div>
+          <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="w-[34.11px] h-[34.11px] left-[821.47px] top-[10.66px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-[14.26px] absolute bg-[#5f1f1c] rounded-full" />
+          <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[14.26px] absolute bg-[#5f1f1c] rounded-full" />
+          <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="w-[34.11px] h-[34.11px] left-[906.75px] top-[184.76px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-[14.26px] absolute bg-[#ff3b30] rounded-full" />
+            <div className="w-[28.42px] h-[28.42px] left-[2.84px] top-[14.26px] absolute bg-[#ff2d55] rounded-full" />
+          </div>
+          <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+        </div>
+        <div className="origin-top-left rotate-90 w-[28.42px] h-[2.57px] left-[77.12px] top-[139.28px] absolute"></div>
+        <div className="w-52 h-[58.74px] left-[73.19px] top-[78.17px] absolute">
+          <div className="w-[100.45px] h-[26px] left-0 top-0 absolute">
+            <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+              2020
+            </div>
+            <div className="left-[50.45px] top-[7.82px] absolute text-[#00c7be] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+              December
+            </div>
+          </div>
+          <div className="left-0 top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+            Initiate revamping the platfrom from <br />
+            v1 to v2
+          </div>
+        </div>
+        <div className="w-[122.45px] h-[76.74px] left-[174.81px] top-[253.69px] absolute">
+          <div className="w-[122.45px] h-[26px] left-0 top-0 absolute">
+            <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+              2020
+            </div>
+            <div className="left-[50.45px] top-[7.82px] absolute text-[#32ade6] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+              December-Mid
+            </div>
+          </div>
+          <div className="w-[120.81px] left-0 top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+            Start Researching and creating moodboard
+          </div>
+        </div>
+        <div className="w-[174.10px] h-[76.74px] left-[309.12px] top-[117.96px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+              <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute bg-[#26394f] rounded-full" />
+              <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[4.26px] absolute bg-[#26394f] rounded-full" />
+            </div>
+            <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+          </div>
+          <div className="w-[131.46px] h-[76.74px] left-[42.64px] top-[-0px] absolute">
+            <div className="w-[82.35px] h-[26px] left-0 top-0 absolute">
+              <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+                2021
+              </div>
+              <div className="left-[43.35px] top-[7.82px] absolute text-[#007aff] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+                January
+              </div>
+            </div>
+            <div className="w-[131.46px] left-[-0px] top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+              Start defining foundations and elements
+            </div>
+          </div>
+        </div>
+        <div className="w-[166.09px] h-[112.74px] left-[309.12px] top-[337.54px] absolute">
+          <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+            <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute">
+              <div className="w-[34.11px] h-[34.11px] left-0 top-0 absolute bg-[#26313e] rounded-full" />
+              <div className="w-[25.58px] h-[25.58px] left-[4.26px] top-[4.26px] absolute bg-[#26323f] rounded-full" />
+            </div>
+            <div className="w-[17.05px] h-[17.05px] left-[8.88px] top-[8.53px] absolute  overflow-hidden" />
+          </div>
+          <div className="w-[123.45px] h-[112.74px] left-[42.64px] top-[-0px] absolute">
+            <div className="w-[123.45px] h-[26px] left-0 top-0 absolute">
+              <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+                2020
+              </div>
+              <div className="left-[50.45px] top-[7.82px] absolute text-[#32ade6] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+                December-End
+              </div>
+            </div>
+            <div className="w-[117.25px] left-[-0px] top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+              Taking Insights from CSM Team, Conducting User Interview to create
+              from scratch
+            </div>
+          </div>
+        </div>
+        <div className="w-[117.25px] h-[76.74px] left-[524.44px] top-[210.34px] absolute">
+          <div className="w-[104.35px] h-[26px] left-0 top-0 absolute">
+            <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+              2021
+            </div>
+            <div className="left-[43.35px] top-[7.82px] absolute text-[#007aff] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+              January-Mid
+            </div>
+          </div>
+          <div className="w-[117.25px] left-0 top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+            Design System is now complete and ready for use
+          </div>
+        </div>
+        <div className="w-[117.25px] h-[76.74px] left-[699.25px] top-[289.93px] absolute">
+          <div className="w-[110.35px] h-[26px] left-0 top-0 absolute">
+            <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+              2021
+            </div>
+            <div className="left-[43.35px] top-[7.82px] absolute text-[#af52de] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+              February-End
+            </div>
+          </div>
+          <div className="w-[117.25px] left-[-0px] top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+            Handovered all new design files to product engineers
+          </div>
+        </div>
+        <div className="w-[117.25px] h-[76.74px] left-[699.25px] top-[90.25px] absolute">
+          <div className="w-[63.35px] h-[26px] left-0 top-0 absolute">
+            <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+              2021
+            </div>
+            <div className="left-[43.35px] top-[7.82px] absolute text-[#af52de] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+              July
+            </div>
+          </div>
+          <div className="w-[117.25px] left-[-0px] top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+            New platform get launched successfully!
+          </div>
+        </div>
+        <div className="w-[117.25px] h-[76.74px] left-[864.11px] top-[10.66px] absolute">
+          <div className="w-[83.35px] h-[26px] left-0 top-0 absolute">
+            <div className="left-0 top-0 absolute text-[#d7dfe7] text-[17.05px] font-bold font-['SF Pro'] leading-relaxed tracking-tight">
+              2021
+            </div>
+            <div className="left-[41.35px] top-[7.82px] absolute text-[#ff2d55] text-[9.95px] font-['SF Pro'] leading-[14.92px] tracking-tight">
+              October
+            </div>
+          </div>
+          <div className="w-[117.25px] left-[-0px] top-[22.74px] absolute text-[#d7dfe7] text-xs font-normal font-['SF Pro'] leading-[18px] tracking-tight">
+            Design System gets a new version upgrade as 2.1
+          </div>
+        </div>
+      </div>
+      <div className="w-[58px] h-[60px] left-0 top-[190px] absolute bg-gradient-to-l from-black to-black" />
+      <div className="w-[58px] h-[60px] left-[1024px] top-[210px] absolute origin-top-left rotate-180 bg-gradient-to-l from-black to-black" />
+    </div>
+  );
+};
+
+export default Journey;
