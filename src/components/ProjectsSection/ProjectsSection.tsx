1dea088cafaea50201025e65ece6fa4eb0cbf9d5
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-31 16:30:13 +0530

    Update ProjectsSection.tsx

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Lock } from "lucide-react";
 import GifBackgroundCard from "../GifBackgroundCard/GifBackgroundCard";
+import qdesq from "../../assets/projects/qdesq.png";
+import wehive from "../../assets/projects/wehive.png";
+import hubilo from "../../assets/projects/hubilo.png";
+import ioscs from "../../assets/projects/ioscs.png";
+import dyt from "../../assets/projects/dyt.png";
 
 const ProjectsSection: React.FC = () => {
   return (
-    <div className="flex flex-col gap-[20px] py-[24px] w-[100%] md:w-[928px]">
+    <div className="flex flex-col gap-[20px] py-[24px] w-[100%] md:w-[1048px]">
       <div className="flex flex-col items-center gap-[20px]">
         <img
           src="https://s3-alpha-sig.figma.com/img/fb53/b23d/fa08b9d0d398c8c13bb0af77f4505c84?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VEyDCd3DI11IytDvy4Ykt8N~2xMqTBPG8jTxT4b7CZJvMguJz39WNlc3rZgnRYtTDFQkMs6Hq6oertfHAQor4Qpcr1s8TkpMeaBymHdV7Ke0ZQFcbu76t3Colrm53EFPOWE5MZRTGKUuup5-VbXCdAQreU-DbOjhnOw8Emw15oKdHgBZVn75VeZIUs-0wiSmGfHvvYWZCOlqAfYXlR37EpZhjwg43JK2zhvTg2~-EeyvqmDmjapkqvqEXLUWtCTPxOfDM1Ek3K73~Wu~aXP0w3Av~4rkXCgp95mPJO8WCMOEVlfi9TZpF68gHEQQQx4uLBSJdlg2WCC0c8zlrUGW6Q__"
@@ -14,48 +19,56 @@ const ProjectsSection: React.FC = () => {
         <h2 className="text-[24px] md:text-[32px] font-semibold">
           Case studies and projects
         </h2>
-        <div className="flex gap-[20px] flex-col md:flex-row">
-          <GifBackgroundCard
-            title="iOS Case Study"
-            description={["UX Correction", "Case Study", "Self"]}
-            gif="https://media.giphy.com/media/ehgM1JRwNXAtaYDRzi/giphy.gif?cid=ecf05e47n16m0tbpdyrsm5l5tuuqiczx1uf2krb1rj31ifrw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-            img="https://images3.alphacoders.com/100/thumb-350-1006667.webp"
-            icon={<Lock size={12} />}
-          />
-          <GifBackgroundCard
-            title="iOS Case Study"
-            description={["UX Correction", "Case Study", "Self"]}
-            gif="https://media.giphy.com/media/ehgM1JRwNXAtaYDRzi/giphy.gif?cid=ecf05e47n16m0tbpdyrsm5l5tuuqiczx1uf2krb1rj31ifrw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-            img="https://images3.alphacoders.com/100/thumb-350-1006667.webp"
-            icon={<Lock size={12} />}
-          />
+        <div className="flex flex-col md:flex-row gap-[20px] flex-wrap w-full justify-center items-center">
+          {ProjectData?.map((project) => (
+            <GifBackgroundCard
+              key={project.title}
+              title={project.title}
+              description={project.description}
+              img={project.img}
+              icon={project.icon}
+            />
+          ))}
         </div>
-        <div className="flex flex-col md:flex-row gap-[20px]">
-          <GifBackgroundCard
-            title="iOS Case Study"
-            description={["UX Correction", "Case Study", "Self"]}
-            gif="https://media.giphy.com/media/ehgM1JRwNXAtaYDRzi/giphy.gif?cid=ecf05e47n16m0tbpdyrsm5l5tuuqiczx1uf2krb1rj31ifrw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-            img="https://images3.alphacoders.com/100/thumb-350-1006667.webp"
-            icon={<Lock size={12} />}
-          />
-          <GifBackgroundCard
-            title="iOS Case Study"
-            description={["UX Correction", "Case Study", "Self"]}
-            gif="https://media.giphy.com/media/ehgM1JRwNXAtaYDRzi/giphy.gif?cid=ecf05e47n16m0tbpdyrsm5l5tuuqiczx1uf2krb1rj31ifrw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-            img="https://images3.alphacoders.com/100/thumb-350-1006667.webp"
-            icon={<Lock size={12} />}
-          />
-        </div>
-        <GifBackgroundCard
-          title="iOS Case Study"
-          description={["UX Correction", "Case Study", "Self"]}
-          gif="https://media.giphy.com/media/ehgM1JRwNXAtaYDRzi/giphy.gif?cid=ecf05e47n16m0tbpdyrsm5l5tuuqiczx1uf2krb1rj31ifrw&ep=v1_gifs_search&rid=giphy.gif&ct=g"
-          img="https://images3.alphacoders.com/100/thumb-350-1006667.webp"
-          icon={<Lock size={12} />}
-        />
       </div>
     </div>
   );
 };
 
 export default ProjectsSection;
+
+type ProjectDataType = {
+  title: string;
+  description: string[];
+  img: string;
+  icon?: React.ReactNode;
+};
+
+const ProjectData: ProjectDataType[] = [
+  {
+    title: "Qdesq",
+    description: ["UX Correction", "Case Study", "Freelance"],
+    img: qdesq,
+  },
+  {
+    title: "WeHive",
+    description: ["Case Study", "Freelance"],
+    img: wehive,
+    icon: <Lock size={12} />,
+  },
+  {
+    title: "iOS Case Study",
+    description: ["UX Correction", "Case Study", "Self"],
+    img: ioscs,
+  },
+  {
+    title: "Hubilo Platform redesign",
+    description: ["UX Correction", "Case Study", "Full Time"],
+    img: hubilo,
+  },
+  {
+    title: "DYT Club",
+    description: ["Case Study", "Freelance"],
+    img: dyt,
+  },
+];
