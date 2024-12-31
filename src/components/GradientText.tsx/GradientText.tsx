4c6a27877f0930f69b39daab57b3bce958132c34
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-31 13:10:58 +0530

    Update GradientText.tsx

--- src/components/GradientText.tsx/GradientText.tsx
+++ src/components/GradientText.tsx/GradientText.tsx
@@ -8,7 +8,7 @@ interface GradientTextProps {
 const GradientText: React.FC<GradientTextProps> = ({ children, className }) => {
   return (
     <span
-      className={`bg-gradient-to-r from-[#E43D19] via-[#3472BC] to-[#06D7D7] bg-clip-text text-transparent ${className}`}
+      className={`bg-gradient-to-r from-[#E43D19] via-[#3472BC] to-[#06D7D7] bg-clip-text text-transparent font-edu ${className}`}
     >
       {children}
     </span>
