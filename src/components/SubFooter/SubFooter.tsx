bb173853cabfb3b81fc931fd343a8ffc09aedcbc
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-01 13:02:32 +0530

    Update SubFooter.tsx

--- src/components/SubFooter/SubFooter.tsx
+++ src/components/SubFooter/SubFooter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SubFooter: React.FC = () => {
   return (
-    <div className="flex justify-center items-baseline md:items-center h-[42px] w-[100%] text-center text-[12px] border-t-[1px] border-zinc-100/50">
+    <div className="flex justify-center items-baseline md:items-center h-[42px] w-[100%] text-center text-[12px] border-t-[1px] border-[#404040]">
       Designed with vision by Jitendra, brought to life by Priyanshu—crafted
       with precision and a touch of <span className="animate-pulse">❤️</span>.
     </div>
