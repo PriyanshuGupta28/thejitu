c572426ac8f9ec4565f2a60544528151e2194613
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-01 13:19:28 +0530

    Update HeroSection.tsx

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import GradientText from "../GradientText.tsx/GradientText";
 import GlowingCard from "../GlowingCard/GlowingCard";
+import logo from "../../assets/logo.png";
 
 const HeroSection: React.FC = () => {
   return (
     <div className="flex py-[24px] flex-col justify-center items-center">
+      <div className="flex flex-col md:hidden py-[12px] justify-center items-center gap-[12px]">
+        <div>
+          <img src={logo} alt="logo" className="w-[22.857px] h-[48px]" />
+        </div>
+        <div>
+          <p className="text-center text-[16px] font-[656] font-compact">
+            Jitendra Kumar
+          </p>
+          <p className="text-center text-[14px] font-compact font-[457] text-white/80">
+            Product Designer
+          </p>
+        </div>
+      </div>
       <GlowingCard />
       <div className="flex flex-col gap-[24px] p-[48px] justify-center items-center ">
         <h1 className="text-[32px] text-center">
