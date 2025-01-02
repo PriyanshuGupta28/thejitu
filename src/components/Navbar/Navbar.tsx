33dae5e8a367146b5e7fbcdd70f3d3d0baee5401
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-02 17:31:52 +0530

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import logo from "../../assets/logo.png";
 import { Menu } from "lucide-react";
+import figma from "../../assets/icons/figma.png";
+import ld from "../../assets/icons/ld.png";
+import medium from "../../assets/icons/medium.png";
 
 const Navbar: React.FC = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -19,37 +22,40 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <nav
-      className={`hidden transition-all duration-300 ease-in-out navbar w-full h-[72px] text-white md:flex items-center justify-between px-[48px] top-0 fixed border-b rounded-b-[20px] ${
-        scrolled
-          ? " bg-black/40 border-zinc-800 backdrop-blur-[20px]"
-          : "bg-transparent border-transparent"
-      }`}
-      style={{ zIndex: 1000 }}
-    >
-      <div className="flex-col hidden md:flex">
-        <p className="text-[16px]">Jitendra Kumar</p>
-        <p className="text-[11px] text-white">Product Designer</p>
-      </div>
-      <div>
-        <img src={logo} alt="logo" className="w-[22.86px] h-[48px]" />
-      </div>
-      <div className="hidden flex-row gap-[10px] md:flex">
-        {navLinks.map((link) => (
-          <Link
-            key={link.title}
-            to={link.link}
-            target="_blank"
-            rel="noreferrer"
-            className="text-[12px] underline"
-          >
-            {link.title}{" "}
-            {navLinks.length - 1 === navLinks.indexOf(link) ? "" : "/"}
-          </Link>
-        ))}
-      </div>
-      <div className="flex md:hidden">
-        <Menu className="w-[24px] h-[24px] cursor-pointer" />
+    <nav className="flex justify-center items-center">
+      <div
+        className={`hidden transition-all duration-300 ease-in-out navbar w-[1028px] h-[72px] text-white md:flex items-center justify-between px-[48px] top-[20px] fixed border rounded-[20px] ${
+          scrolled
+            ? " bg-black/40 border-zinc-800 backdrop-blur-[20px]"
+            : "bg-transparent border-transparent"
+        }`}
+        style={{ zIndex: 1000 }}
+      >
+        <div className="flex-col hidden md:flex">
+          <p className="text-[16px]">Jitendra Kumar</p>
+          <p className="text-[11px] text-white/80">Product Designer</p>
+        </div>
+        <div>
+          <img src={logo} alt="logo" className="w-[22.86px] h-[48px]" />
+        </div>
+        <div className="hidden flex-row gap-[10px] md:flex">
+          {navLinks.map((link) => (
+            <Link
+              key={link.title}
+              to={link.link}
+              target="_blank"
+              rel="noreferrer"
+              className="text-[12px] underline"
+            >
+              {/* {link.title}{" "}
+              {navLinks.length - 1 === navLinks.indexOf(link) ? "" : "/"} */}
+              <img src={link.icon} alt="alt" />
+            </Link>
+          ))}
+        </div>
+        <div className="flex md:hidden">
+          <Menu className="w-[24px] h-[24px] cursor-pointer" />
+        </div>
       </div>
     </nav>
   );
@@ -59,15 +65,18 @@ export default Navbar;
 
 const navLinks = [
   {
-    title: "Figma",
-    link: "https://www.figma.com/@jitkrgupta",
-  },
-  {
     title: "LinkedIn",
+    icon: ld,
     link: "https://www.linkedin.com/in/jitkrgupta",
   },
   {
+    title: "Figma",
+    icon: figma,
+    link: "https://www.figma.com/@jitkrgupta",
+  },
+  {
     title: "Medium",
+    icon: medium,
     link: "https://jitkrgupta.medium.com",
   },
 ];
