752e27f36536e2d264334a1de30edc823b30bca7
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-05 13:56:47 +0530

    Update AllRoutes.tsx

--- src/Routes/AllRoutes.tsx
+++ src/Routes/AllRoutes.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Routes, Route } from "react-router";
 import Home from "../pages/Home/Home";
 import Ios from "../pages/Ios/Ios";
+import Hubilo from "../pages/Hubilo/Hubilo";
+import ScrollRestoration from "../components/ScrollRestoration/ScrollRestoration";
 
 const AllRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/ios" element={<Ios />} />
-    </Routes>
+    <>
+      <ScrollRestoration />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ios" element={<Ios />} />
+        <Route path="/hubilo" element={<Hubilo />} />
+      </Routes>
+    </>
   );
 };
 
