d3bf0f92f15e9c26f46dc59eba094622950a346b
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-06 18:25:24 +0530

    Create Solution.tsx

---
+++ src/components/Hubilio/Solution.tsx
@@ -0,0 +1,289 @@
+import React from "react";
+import sol1 from "../../assets/sol1.png";
+import sol2 from "../../assets/sol2.png";
+import sol3 from "../../assets/sol3.png";
+import sol4 from "../../assets/sol4.png";
+
+const Solution: React.FC = () => {
+  return (
+    <div className="h-[1418px] p-12 flex-col justify-center items-center gap-6 inline-flex">
+      <div className="w-[1024px] justify-start items-start gap-4 inline-flex">
+        <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+          Solution
+        </div>
+        <div className="grow shrink basis-0 flex-col justify-center items-center gap-9 inline-flex">
+          <div className="self-stretch ">
+            <span className="text-white text-sm font-normal font-text leading-normal">
+              To address the challenges identified, we adopted a{" "}
+            </span>
+            <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+              user-centered approach
+            </span>
+            <span className="text-white text-sm font-normal font-text leading-normal">
+              —focusing on simplifying the platform for{" "}
+            </span>
+            <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+              first-time users
+            </span>
+            <span className="text-white text-sm font-normal font-text leading-normal">
+              {" "}
+              while providing{" "}
+            </span>
+            <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+              customization flexibility for clients.
+            </span>
+          </div>
+          <div className="self-stretch h-[330px] p-[1px] bg-border-fade rounded-[36px]">
+            <div className="self-stretch h-[330px] p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 flex relative">
+              <div className="text-white text-base font-semibold font-['SF Pro Display']">
+                Research & Insights – Unlocking User Needs
+              </div>
+              <div className="self-stretch">
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  User interviews played a critical role in shaping the design
+                  strategy. Due to{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  NDA restrictions,
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  interviews were conducted with:
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  CAB Members (Customer Advisory Board)
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – Provided in-depth insights into client requirements,
+                  branding expectations, and functional gaps.
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  Recently Onboarded Users
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – Offered fresh perspectives on first-time usability,
+                  highlighting areas where navigation and workflows caused
+                  friction.
+                  <br />
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  <br />
+                  These insights helped me define{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  concepts and hypotheses,
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  ensuring the platform addressed the{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  unique needs
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  of both audiences—users and clients.
+                </span>
+              </div>
+              <img
+                className="w-[72px] h-[72px] left-[30px] top-[-36.28px] absolute"
+                src={sol1}
+              />
+            </div>
+          </div>
+          <div className="self-stretch h-[403px] p-[1px] bg-border-fade rounded-[36px]">
+            <div className="self-stretch h-[403px] p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 flex relative">
+              <div className="text-white text-base font-semibold font-['SF Pro Display']">
+                Conceptualization & Prototyping – From Ideas to Action
+              </div>
+              <div className="self-stretch">
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  Armed with{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  qualitative data,
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  I led the team through the{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  conceptualization phase,
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  focusing on:
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  Simplified Navigation
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – Designed an interface that reduced clicks, displayed{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  key features upfront,
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  and enabled multi-tasking during live events.
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  Customization Freedom
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – Introduced modular components and{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  custom theming options,
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  allowing clients to adapt designs to match their branding
+                  guidelines without restrictions.
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  Engagement Features
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – Integrated
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  {" "}
+                  gamification elements
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  to drive user participation and make virtual events more
+                  dynamic and interactive.
+                  <br />
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  <br />
+                  Prototypes were developed to visualize these concepts, and
+                  each design was put through a{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  feedback loop{" "}
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  involving:
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  User Testing with CAB Members
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – To validate business requirements and client expectations.
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  Usability Testing with New Users
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  – To evaluate ease of onboarding, navigation clarity, and
+                  overall experience.
+                </span>
+              </div>
+              <img
+                className="w-[72px] h-[72px] left-[30px] top-[-36px] absolute"
+                src={sol2}
+              />
+            </div>
+          </div>
+          <div className="self-stretch h-[235px] p-[1px] bg-border-fade rounded-[36px]">
+            <div className="self-stretch h-[235px] p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 flex relative">
+              <div className="text-white text-base font-semibold font-['SF Pro Display']">
+                Refinement & Validation – Iterating Toward Perfection
+              </div>
+              <div className="self-stretch">
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  The feedback gathered during testing allowed me to:
+                  <br />
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  Refine layouts{" "}
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  for better accessibility and responsiveness.
+                  <br />
+                  Optimize workflows to{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  reduce onboarding time
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  and improve task efficiency.
+                  <br />
+                  Ensure the{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  design system
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  supported both{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  customization and consistency
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  —meeting the flexibility needs of clients without compromising
+                  usability.
+                </span>
+              </div>
+              <img
+                className="w-[72px] h-[72px] left-[30px] top-[-36px] absolute"
+                src={sol3}
+              />
+            </div>
+          </div>
+          <div className="self-stretch h-[162px] p-[1px] bg-border-fade rounded-[36px]">
+            <div className="self-stretch h-[162px] p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 flex relative">
+              <div className="text-white text-base font-semibold font-['SF Pro Display']">
+                Result?
+              </div>
+              <div className="self-stretch">
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  A platform that felt
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  {" "}
+                  intuitive for new users
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  {" "}
+                  while giving{" "}
+                </span>
+                <span className="text-[#00c7be] text-sm font-bold font-kalam leading-normal">
+                  clients the freedom to customize
+                </span>
+                <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                  —positioning Hubilo as a market leader in virtual events.
+                </span>
+              </div>
+              <img
+                className="w-[72px] h-[72px] left-[30px] top-[-36px] absolute"
+                src={sol4}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Solution;
