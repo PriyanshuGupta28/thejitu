a4baccfcfc5f4ca6b969aac58f81ada135c8c258
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-07 00:08:21 +0530

    Create FinalTakeaway.tsx

---
+++ src/components/Hubilio/FinalTakeaway.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+const FinalTakeaway: React.FC = () => {
+  return (
+    <div className="h-[264px] p-12 flex-col justify-center items-center gap-6 inline-flex">
+      <div className="w-[1024px] justify-start items-start gap-4 inline-flex">
+        <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+          Final Takeaway
+        </div>
+        <div className="grow shrink basis-0 self-stretch">
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            Throughout this project, I gained{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            valuable insights
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            into the challenges of designing for{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            virtual event platforms{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            and how{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            scalable solutions
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            drive growth. Collaborating closely with{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            stakeholders,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            I designed dynamic tools like the{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            customizable side panel{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            and{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            theme editor, addressing real user needs
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            and setting new industry benchmarks.
+            <br />
+            <br />
+            Additionally, I contributed to building a{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            design system
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            that improved{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            efficiency,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            ensured{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            consistency,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            and empowered clients to create personalized experiences. This
+            project not only helped Hubilo achieve{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            40% revenue growth
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            but also influenced{" "}
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            competitors
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            to follow suit—reinforcing Hubilo’s position as an
+          </span>
+          <span className="text-[#fbff67] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            industry leader
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            in virtual events.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FinalTakeaway;
