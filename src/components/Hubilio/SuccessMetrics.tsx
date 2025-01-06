186e81fcbd0c728f97725e8e8ede65296ba7fd1a
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-07 00:08:07 +0530

    Create SuccessMetrics.tsx

---
+++ src/components/Hubilio/SuccessMetrics.tsx
@@ -0,0 +1,173 @@
+import React from "react";
+
+const SuccessMetrics: React.FC = () => {
+  return (
+    <div className="h-[623px] p-12 flex-col justify-center items-center gap-6 inline-flex">
+      <div className="w-[1024px] justify-start items-start gap-4 inline-flex">
+        <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+          Success Metrics
+        </div>
+        <div className="grow shrink basis-0 flex-col justify-center items-center gap-4 inline-flex">
+          <div className="self-stretch justify-start items-start gap-4 inline-flex">
+            <div className="grow shrink basis-0 self-stretch p-[1px] bg-border-fade rounded-[36px]">
+              <div className="grow shrink basis-0 self-stretch p-9 bg-light-to-dark rounded-[36px] flex-col justify-start items-start gap-6 inline-flex">
+                <div className="self-stretch text-white text-base font-semibold font-['SF Pro Display'] leading-7">
+                  Goal
+                </div>
+                <div className="self-stretch">
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    Establish Hubilo as a{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    market{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    leader
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    by offering{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    exclusive features
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    unavailable elsewhere.
+                    <br />
+                    Deliver{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    on-demand customization
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    and dynamic experiences to attract and retain clients.
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 p-[1px] bg-border-fade rounded-[36px]">
+              <div className="grow shrink basis-0 p-9 bg-light-to-dark rounded-[36px] flex-col justify-start items-start gap-6 inline-flex">
+                <div className="self-stretch text-white text-base font-semibold font-['SF Pro Display'] leading-7">
+                  Metric
+                </div>
+                <div className="self-stretch">
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    Achieved a{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    40% increase in revenue
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    through feature-driven growth and{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    client retention.
+                    <br />
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    The{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    industry-first dynamic side panel{" "}
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    became a{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    game-changer,
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    later{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    adopted
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    by competitors like{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    Hopin, Airmeet,
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                    {" "}
+                    and{" "}
+                  </span>
+                  <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                    Zuddl.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch h-[267px] p-[1px] bg-border-fade rounded-[36px]">
+            <div className="self-stretch h-[267px] p-9 bg-light-to-dark rounded-[36px] flex-col justify-start items-start gap-6 flex">
+              <div className="self-stretch text-white text-base font-semibold font-['SF Pro Display'] leading-7">
+                What will users accomplish using this feature and how?
+              </div>
+              <div className="self-stretch">
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  Personalization at scale
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  {" "}
+                  with custom themes, layouts, and branding options.
+                  <br />
+                </span>
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  Increased engagement
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  {" "}
+                  through interactive tools, polls, and gamification features.
+                  <br />
+                </span>
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  Efficient navigation{" "}
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  with modular designs and dynamic side panels for better
+                  accessibility.
+                  <br />
+                </span>
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  Simplified workflows
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  {" "}
+                  for organizers, enabling{" "}
+                </span>
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  faster event setups{" "}
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  and flexible content management.
+                  <br />
+                </span>
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  Data-driven insights
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  {" "}
+                  with built-in analytics, empowering organizers to{" "}
+                </span>
+                <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                  track performance{" "}
+                </span>
+                <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                  and optimize events effectively.
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessMetrics;
