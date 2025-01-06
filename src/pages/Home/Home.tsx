3f772145d8fb89a5f4d0691663f777f9789f44b8
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-06 18:26:40 +0530

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -10,6 +10,14 @@ const Home: React.FC = () => {
       <div className="flex flex-col items-center justify-center py-16">
         <HeroSection />
         <Skills />
+        <img
+          src="https://s3-alpha-sig.figma.com/img/fb53/b23d/fa08b9d0d398c8c13bb0af77f4505c84?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VEyDCd3DI11IytDvy4Ykt8N~2xMqTBPG8jTxT4b7CZJvMguJz39WNlc3rZgnRYtTDFQkMs6Hq6oertfHAQor4Qpcr1s8TkpMeaBymHdV7Ke0ZQFcbu76t3Colrm53EFPOWE5MZRTGKUuup5-VbXCdAQreU-DbOjhnOw8Emw15oKdHgBZVn75VeZIUs-0wiSmGfHvvYWZCOlqAfYXlR37EpZhjwg43JK2zhvTg2~-EeyvqmDmjapkqvqEXLUWtCTPxOfDM1Ek3K73~Wu~aXP0w3Av~4rkXCgp95mPJO8WCMOEVlfi9TZpF68gHEQQQx4uLBSJdlg2WCC0c8zlrUGW6Q__"
+          alt="jituthumb_img"
+          className="w-[79.65px] h-[90px] mt-5"
+        />
+        <h2 className="text-[24px] md:text-[32px] font-semibold tracking-[-0.32px]">
+          Case studies and projects
+        </h2>
         <ProjectsSection />
         <div>
           <div className="flex justify-center items-center py-[72px]">
