42f53da236f02ec70d360e4626842ed8e3c66079
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-06 18:25:15 +0530

    Create ProblemStatements.tsx

---
+++ src/components/Hubilio/ProblemStatements.tsx
@@ -0,0 +1,192 @@
+import React from "react";
+
+const ProblemStatements: React.FC = () => {
+  return (
+    <div className="h-[950px] p-12 flex-col justify-center items-center gap-6 inline-flex">
+      <div className="w-[1024px] justify-start items-start gap-4 inline-flex">
+        <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+          Problem <br />
+          Statements
+        </div>
+        <div className="grow shrink basis-0 flex-col justify-center items-center gap-4 inline-flex">
+          <div className="self-stretch">
+            <span className="text-white/80 text-sm font-normal font-text leading-normal">
+              When I joined Hubilo, one of the biggest challenges was{" "}
+            </span>
+            <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+              understanding user behavior
+            </span>
+            <span className="text-white/80 text-sm font-normal font-text leading-normal">
+              —particularly for a platform designed to host{" "}
+            </span>
+            <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+              virtual events
+            </span>
+            <span className="text-white/80 text-sm font-normal font-text leading-normal">
+              {" "}
+              with{" "}
+            </span>
+            <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+              unique visitors
+            </span>
+            <span className="text-white/80 text-sm font-normal font-text leading-normal">
+              {" "}
+              who may not return after a single event.
+            </span>
+          </div>
+          <div className="self-stretch justify-start items-start gap-4 inline-flex ">
+            <div className="grow shrink basis-0 self-stretch p-[1px] rounded-[36px] bg-border-fade">
+              <div className="grow shrink basis-0 self-stretch p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 inline-flex h-full">
+                <div className="w-[220px] text-white text-base font-semibold font-['SF Pro Display']">
+                  For Users
+                </div>
+                <div className="self-stretch">
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    The platform needed to:
+                    <br />
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    Be intuitive and{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    easy-to-use
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    for{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    first-time users
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    unfamiliar with its functionality.
+                    <br />
+                    Provide a{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    glanceable interface
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    showcasing all essential features upfront to avoid confusion
+                    during live events.
+                    <br />
+                    Incorporate{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    gamification
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    and engagement tools to keep audiences active and involved.
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="grow shrink basis-0 p-[1px] bg-border-fade rounded-[36px]">
+              <div className="grow shrink basis-0 p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 inline-flex">
+                <div className="w-[220px] text-white text-base font-semibold font-['SF Pro Display']">
+                  For Clients
+                </div>
+                <div className="self-stretch">
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    The platform had to:
+                    <br />
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    Offer{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    extensive customization options
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    —allowing clients to design experiences that aligned with
+                    their{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    branding
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    and{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    style guidelines
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    without restrictions.
+                    <br />
+                    Provide{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    flexibility
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    to use any component or element to meet diverse needs—even
+                    if it risked visual clutter—ensuring clients had the{" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    freedom to experiment,
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    a feature most competitors lacked.
+                    <br />
+                    Support
+                  </span>
+                  <span className="text-[#34c759] text-sm font-normal font-text leading-normal">
+                    {" "}
+                  </span>
+                  <span className="text-[#34c759] text-sm font-bold font-kalam leading-normal">
+                    custom theming
+                  </span>
+                  <span className="text-white/80 text-sm font-normal font-text leading-normal">
+                    {" "}
+                    to adapt to varied client requirements and event types while
+                    maintaining usability.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch h-[347px] p-[1px] bg-border-fade rounded-[36px]">
+            <div className="self-stretch h-[347px] p-9 bg-gradient-to-b from-[#101010] to-black rounded-[36px] flex-col justify-start items-start gap-6 flex ">
+              <div className="w-[220px] text-white text-base font-semibold font-['SF Pro Display']">
+                Target Audience
+              </div>
+              <div className="self-stretch text-white/80 text-sm font-normal font-text leading-normal">
+                The platform has usually newer audience as new events get
+                begins.
+              </div>
+              <div className="self-stretch justify-center items-end gap-9 inline-flex ">
+                <div className="flex-col justify-center items-start gap-2.5 inline-flex relative">
+                  <div className="left-[-3.50px] top-[207px] absolute text-center text-white/80 text-xs font-normal font-['SF Pro'] whitespace-nowrap">
+                    Unique Users
+                  </div>
+                  <div className="w-[69.50px] text-center text-white text-xs font-normal font-['SF Pro']">
+                    80%
+                  </div>
+                  <div className="w-[69.50px] h-[175px] bg-[#ff9500] rounded-lg" />
+                </div>
+                <div className="flex-col justify-center items-start gap-2.5 inline-flex relative">
+                  <div className="w-[94.08px] left-[-12px] top-[52px] absolute text-center text-white/80 text-xs font-normal font-['SF Pro']">
+                    Return Users
+                  </div>
+                  <div className="w-[69.50px] text-center text-white text-xs font-normal font-['SF Pro']">
+                    20%
+                  </div>
+                  <div className="w-[69px] h-5 bg-[#007aff] rounded-lg" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProblemStatements;
