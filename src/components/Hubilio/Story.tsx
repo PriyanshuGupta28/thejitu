7604eefe8b64853ef7799e34337831ab0af2d21d
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-06 18:25:00 +0530

    Create Story.tsx

---
+++ src/components/Hubilio/Story.tsx
@@ -0,0 +1,279 @@
+import React from "react";
+import Union1 from "../../assets/Union1.png";
+import Union2 from "../../assets/Union2.png";
+import Union3 from "../../assets/Union3.png";
+import Tailor from "../../assets/Tailor.png";
+import Rajesh from "../../assets/Rajesh.png";
+import Ajit from "../../assets/Ajit.png";
+
+const Story: React.FC = () => {
+  return (
+    <div className="w-full flex flex-col justify-center items-center gap-3 py-[48px]">
+      <div className="w-[1024px] h-[329px] p-12 bg-gradient-to-b from-white to-[#f2f2f5] rounded-[48px] border border-neutral-200 flex-col justify-start items-start gap-12 inline-flex overflow-hidden">
+        <div className="self-stretch h-[233px] flex-col justify-start items-start gap-3 flex">
+          <div className="text-black text-2xl font-semibold font-['SF Pro Display']">
+            Here, the story begins...
+          </div>
+          <div className="self-stretch">
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              In late 2020, I was working at{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              DoYourThng
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              , focused on scaling designs for creators, when an unexpected call
+              from{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              Hubilo
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              {" "}
+              changed everything.
+              <br />
+              <br />
+              The company was on the verge of something big—
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              a massive funding round{" "}
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              and a{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              bold pivot{" "}
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              to become an{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              all-in-one virtual event platform
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              . During my conversation with{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              Vaibhav Jain
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              , Hubilo’s CEO, he outlined their ambitious vision—
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              to redefine the future of virtual events{" "}
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              and go head-to-head with established giants like Zoom, Hopin,
+              Cvent, and other event organizers.
+              <br />
+              <br />
+              COVID had shifted the world online, and Hubilo was ready to seize
+              the moment—to create a{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              new era of virtual workspaces
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              {" "}
+              that weren’t just functional but also{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              engaging, seamless, and future-proof.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="w-[1024px] h-52 p-12 bg-gradient-to-b from-white to-[#f2f2f5] rounded-[48px] border border-neutral-200 flex-col justify-start items-start gap-12 inline-flex overflow-hidden">
+        <div className="self-stretch h-28 flex-col justify-start items-start gap-3 flex">
+          <div className="text-black text-2xl font-semibold font-['SF Pro Display']">
+            The Mission?
+          </div>
+          <div className="self-stretch">
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              Vaibhav entrusted me with a critical task—
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              to lead the design transformation
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              . My goal was to craft an experience that not only catered to
+              every{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              nuance of a great virtual platform
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              {" "}
+              but also stood{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              distinctly apart
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              {" "}
+              from the competition. It had to be scalable, intuitive, and
+              visually stunning—a product that delivered both power and
+              simplicity.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="w-[1024px] h-[482px] p-12 bg-gradient-to-b from-white to-[#f2f2f5] rounded-[48px] border border-neutral-200 flex-col justify-start items-start gap-12 inline-flex ">
+        <div className="self-stretch h-[386px] flex-col justify-start items-start gap-3 flex">
+          <div className="self-stretch text-black text-2xl font-semibold font-['SF Pro Display'] ">
+            The Team. The Dream.
+          </div>
+          <div className="self-stretch py-6 justify-center items-center gap-6 inline-flex relative">
+            <div className="w-[150px] flex-col justify-center items-center gap-2.5 inline-flex">
+              <div className="w-[100px] h-[100px] bg-[#acc3a8] rounded-[500px] flex-col justify-center items-center gap-1.5 flex overflow-hidden">
+                <img src={Tailor} alt="Tailor" />
+              </div>
+              <div className="self-stretch h-8 flex-col justify-center items-center flex">
+                <div className="text-black text-sm font-['SF Pro']">
+                  Kaushal Tailor
+                </div>
+                <div className="text-right text-black/80 text-[11px] font-['SF Compact']">
+                  Senior Product Designer
+                </div>
+              </div>
+            </div>
+            <div className="w-[150px] flex-col justify-center items-center gap-2.5 inline-flex">
+              <div className="w-[100px] h-[100px] bg-[#d7b384] rounded-[500px] flex-col justify-center items-center gap-1.5 flex overflow-hidden">
+                <img src={Rajesh} alt="Rajesh" />
+              </div>
+              <div className="self-stretch h-8 flex-col justify-center items-center flex">
+                <div className="text-black text-sm font-['SF Pro']">
+                  Rajesh Dharajiya
+                </div>
+                <div className="text-right text-black/80 text-[11px] font-['SF Compact']">
+                  Product Designer
+                </div>
+              </div>
+            </div>
+            <div className="w-[150px] flex-col justify-center items-center gap-2.5 inline-flex">
+              <div className="w-[100px] h-[100px] bg-[#9ed7d9] rounded-[500px] flex-col justify-center items-center gap-1.5 flex overflow-hidden">
+                <img src={Ajit} alt="Ajit" />
+              </div>
+              <div className="self-stretch h-8 flex-col justify-center items-center flex">
+                <div className="text-black text-sm font-['SF Pro']">
+                  Ajit Gudekar
+                </div>
+                <div className="text-right text-black/80 text-[11px] font-['SF Compact']">
+                  Junior Product Designer
+                </div>
+              </div>
+            </div>
+            <div className="w-[334px] h-[130px] left-[-109px] top-[-0.28px] absolute bg-[#d2823b] rounded-2xl shadow-[0px_12px_24px_0px_rgba(0,0,0,0.25)] justify-start items-start gap-2.5 flex">
+              <div className="grow shrink basis-0 h-[123px] p-3  justify-center items-center gap-2.5 flex">
+                <div className="grow shrink basis-0">
+                  <span className="text-white text-sm font-normal font-text leading-tight">
+                    I’ve been with Hubilo since my
+                  </span>
+                  <span className="text-[#665246] text-sm font-bold font-kalam leading-tight">
+                    {" "}
+                    internship days,
+                  </span>
+                  <span className="text-white text-sm font-normal font-text leading-tight">
+                    {" "}
+                    growing alongside the company and shaping its design
+                    foundations. From early layouts to scalable workflows, I’ve
+                    focused on building designs that last.
+                  </span>
+                </div>
+              </div>
+              <img
+                className="w-[27.93px] h-[28.44px] left-[315px] top-[100px] absolute"
+                src={Union1}
+                alt="Union1"
+              />
+            </div>
+            <div className="h-[123px] w-[258.03px] left-[448.48px] top-[-95.67px] absolute origin-top-left rotate-[-10deg] bg-[#665246] rounded-2xl shadow-[0px_12px_24px_0px_rgba(0,0,0,0.25)] justify-start items-start gap-2.5 flex">
+              <img
+                className="w-[27.93px] h-[28.44px] left-[-4px] top-[94px] absolute"
+                src={Union2}
+                alt="Union2"
+              />
+              <div className="grow shrink basis-0 h-[123px] p-3  justify-center items-center gap-2.5 flex z-[10]">
+                <div className="grow shrink basis-0">
+                  <span className="text-white text-sm font-normal font-text leading-tight">
+                    With over{" "}
+                  </span>
+                  <span className="text-[#ffcc00] text-sm font-bold font-kalam leading-tight">
+                    4 years
+                  </span>
+                  <span className="text-white text-sm font-normal font-text leading-tight">
+                    {" "}
+                    at Hubilo, I’ve led designs for the dashboard and core
+                    features. My focus has always been creating intuitive
+                    experiences and simplifying complexity.
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="h-[123px] w-[300px] left-[701px] top-[-7.28px] absolute bg-[#464541] rounded-2xl shadow-[0px_12px_24px_0px_rgba(0,0,0,0.25)] justify-start items-start gap-2.5 flex">
+              <img
+                className="w-[27.93px] h-[28.44px] left-[-8px] top-[94px] absolute"
+                src={Union3}
+              />
+              <div className="grow shrink basis-0 h-[123px] p-3 justify-center items-center gap-2.5 flex z-[10]">
+                <div className="grow shrink basis-0">
+                  <span className="text-white text-sm font-normal font-text leading-tight">
+                    As the{" "}
+                  </span>
+                  <span className="text-[#97cdcf] text-sm font-bold font-kalam leading-tight">
+                    newest member
+                  </span>
+                  <span className="text-white text-sm font-normal font-text leading-tight">
+                    , I’m here to collaborate and help shape Hubilo’s next
+                    chapter. From refining ideas to building prototypes, I’m
+                    focused on pushing boundaries.
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="self-stretch">
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              I joined forces with{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              3 talented designers
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              , and together we set out to shape the future of Hubilo. From
+              defining workflows to reimagining interactions, we were determined
+              to create a platform that would leave an impact.
+              <br />
+              <br />
+              This case study unpacks the journey—from
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              {" "}
+              ideation to execution
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              , the challenges we faced, and the solutions that set Hubilo
+              apart. It’s the story of how design not only solved problems but
+              also{" "}
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              transformed Hubilo into a market leader
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              , earning it
+            </span>
+            <span className="text-[#5856d6] text-sm font-bold font-kalam leading-normal">
+              {" "}
+              $125 million in funding{" "}
+            </span>
+            <span className="text-black text-sm font-normal font-text leading-normal">
+              and a top spot in G2 rankings.
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Story;
