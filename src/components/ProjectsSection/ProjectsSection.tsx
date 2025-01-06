4d4fa403e6eafd21fece96f4fdd7a5bc2e233eef
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-06 18:26:16 +0530

    Update ProjectsSection.tsx

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -11,14 +11,6 @@ const ProjectsSection: React.FC = () => {
   return (
     <div className="flex flex-col gap-[20px] py-[24px] w-[100%] md:w-[1048px]">
       <div className="flex flex-col items-center gap-[20px]">
-        <img
-          src="https://s3-alpha-sig.figma.com/img/fb53/b23d/fa08b9d0d398c8c13bb0af77f4505c84?Expires=1736121600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VEyDCd3DI11IytDvy4Ykt8N~2xMqTBPG8jTxT4b7CZJvMguJz39WNlc3rZgnRYtTDFQkMs6Hq6oertfHAQor4Qpcr1s8TkpMeaBymHdV7Ke0ZQFcbu76t3Colrm53EFPOWE5MZRTGKUuup5-VbXCdAQreU-DbOjhnOw8Emw15oKdHgBZVn75VeZIUs-0wiSmGfHvvYWZCOlqAfYXlR37EpZhjwg43JK2zhvTg2~-EeyvqmDmjapkqvqEXLUWtCTPxOfDM1Ek3K73~Wu~aXP0w3Av~4rkXCgp95mPJO8WCMOEVlfi9TZpF68gHEQQQx4uLBSJdlg2WCC0c8zlrUGW6Q__"
-          alt="jituthumb_img"
-          className="w-[79.65px] h-[90px]"
-        />
-        <h2 className="text-[24px] md:text-[32px] font-semibold tracking-[-0.32px]">
-          Case studies and projects
-        </h2>
         <div className="flex flex-col md:flex-row gap-[20px] flex-wrap w-full justify-center items-center">
           {ProjectData?.map((project) => (
             <GifBackgroundCard
