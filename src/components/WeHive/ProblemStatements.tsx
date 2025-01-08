77537835057ed832d2a873d4d3a6bd789d18cb3e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-08 11:09:05 +0530

    Create ProblemStatements.tsx

---
+++ src/components/WeHive/ProblemStatements.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const ProblemStatements: React.FC = () => {
+  return (
+    <div className="w-[1024px] h-[159px] justify-start items-start gap-4 inline-flex">
+      <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+        Problem <br />
+        Statements
+      </div>
+      <div className="grow shrink basis-0 self-stretch">
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          Managing{" "}
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          multiple projects{" "}
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          in the
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          {" "}
+          interior design industry
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          often involves scattered tools, leading to
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          {" "}
+          complex workflows, miscommunication,
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          and
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          {" "}
+          delays.
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          Existing platforms lack the flexibility and customization required to
+          adapt to the{" "}
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          specific needs
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          of interior designers.
+          <br />
+          The challenge was to create a{" "}
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          centralized solution
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          that simplifies{" "}
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          project tracking, team collaboration,
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          and{" "}
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          task management{" "}
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          without compromising on adaptability—a platform designed to streamline
+          workflows and support{" "}
+        </span>
+        <span className="text-[#34c759] text-sm font-bold font-['Kalam'] leading-normal">
+          scalability
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          for the evolving demands of the industry.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default ProblemStatements;
