567de75f669984be73f1340e6817c53f3c30ae21
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-08 11:09:13 +0530

    Create Research&Design.tsx

---
+++ src/components/WeHive/Research&Design.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import mindtree from "../../assets/wehive/mindtree.png";
+
+const ResearchDesign: React.FC = () => {
+  return <>ResearchDesign</>;
+};
+
+export default ResearchDesign;
