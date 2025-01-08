069de2c31e873793b162a330a2fa06f7abc1bf6c
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-08 11:09:01 +0530

    Create MyRole.tsx

---
+++ src/components/WeHive/MyRole.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+
+const MyRole: React.FC = () => {
+  return (
+    <div className="w-[1024px] h-56 justify-start items-start gap-4 inline-flex">
+      <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+        My Role
+      </div>
+      <div className="grow shrink basis-0 self-stretch">
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          In the{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          WeHive project,
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          I took the lead in shaping its{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          brand identity{" "}
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          and establishing the{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          design foundation
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          to ensure a{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          cohesive visual language
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          across the platform. My role extended beyond aesthetics—I built the
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          {" "}
+          design system
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          from the ground up, focusing on scalability and consistency to support
+          future growth.
+          <br />A key aspect of my work involved designing the{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          dashboard experience,
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          which was centered around{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          role-based access.
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          This approach enabled custom workflows, ensuring that different
+          users—whether{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          admins, managers,
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          or{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          team members
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          —had access to personalized tools and features tailored to their
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          {" "}
+          specific responsibilities.
+          <br />
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          From defining{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          color schemes
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          and{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          typography
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          {" "}
+          to creating{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          modular components,{" "}
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          my focus was on building a system that was both functional and
+          adaptable, empowering teams to{" "}
+        </span>
+        <span className="text-[#ff75b0] text-sm font-bold font-['Kalam'] leading-normal">
+          streamline operations{" "}
+        </span>
+        <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+          without sacrificing flexibility.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default MyRole;
