a6fe93c388db1e439a2433170973006ccc288ff8
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-11 12:52:49 +0530

    Create Overview.tsx

---
+++ src/components/Qdesq/Overview.tsx
@@ -0,0 +1,125 @@
+import React from "react";
+
+const Overview: React.FC = () => {
+  return (
+    <div className="h-80 p-12 flex-col justify-center items-center gap-6 inline-flex">
+      <div className="w-[1024px] justify-start items-start gap-4 inline-flex">
+        <div className="w-[220px] text-white text-2xl font-semibold font-['SF Pro Display']">
+          Overview
+        </div>
+        <div className="grow shrink basis-0">
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            Qdesq is a leading platform for{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            workspace discovery and bookings,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            simplifying the way businesses find and manage office spaces.
+            Designed to cater to the{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            modern workforce,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            Qdesq connects users with
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            flexible workspaces, meeting rooms,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            and{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            co-working solutions
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            —all through a{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            seamless and intuitive interface.
+            <br />
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            Despite its strong market presence, Qdesq faced challenges with an{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            outdated design
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            and
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            cluttered user experience,{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            making navigation and information accessibility
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            complex.
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            With{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            growing competition
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            and the need for a{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            data-savvy
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            approach, the goal was to{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            reimagine its interface
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            to focus on{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            minimalism, usability,{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            and{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            visual clarity.
+            <br />
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            The redesign aimed to deliver a{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            modern, content-rich experience
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            —one that balances form and function to help users make{" "}
+          </span>
+          <span className="text-[#ffcc00] text-sm font-bold font-['Kalam'] leading-normal">
+            quick, informed decisions
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            while enabling Qdesq to stay ahead of industry trends.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
