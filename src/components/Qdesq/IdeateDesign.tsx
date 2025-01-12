fdd1c28fb635d7d2745b35e44b6b3ae31aa47797
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-12 12:43:50 +0530

    Create IdeateDesign.tsx

---
+++ src/components/Qdesq/IdeateDesign.tsx
@@ -0,0 +1,185 @@
+import React from "react";
+import id1 from "../../assets/Qdesq/id1.png";
+import id2 from "../../assets/Qdesq/id2.png";
+import id3 from "../../assets/Qdesq/id3.png";
+
+const IdeateDesign: React.FC = () => {
+  return (
+    <div className="w-[1026px] h-[848px] p-[1px] my-12 bg-border-fade rounded-[48px]">
+      <div className="w-[1024px] h-[848px] p-12  bg-light-to-dark rounded-[48px] flex-col justify-start items-start gap-6 inline-flex">
+        <div className="self-stretch text-white text-2xl font-semibold font-['SF Pro Display'] leading-7">
+          Ideate & Design
+        </div>
+        <div className="self-stretch">
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            The redesign process for Qdesq began with a deep analysis of the{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            existing website, identifying gaps in usability, visual hierarchy,{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            and content flow. Recognizing the increasing reliance on{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            mobile-first experiences,
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            we conducted a{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            competitive analysis{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            of platforms like MyHQ, Cofynd, Regus, and HubbleHQ to benchmark
+            features and industry standards.
+            <br />
+            <br />
+            During{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            brainstorming
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            sessions,{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            Lavesh
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            emphasized prioritizing mobile design, ensuring the layout and
+            interactions were optimized for{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            smaller screens first{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            and then
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            adapted seamlessly{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            to the web experience.
+          </span>
+        </div>
+        <div className="w-[417px]">
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            With this approach, we proposed design changes focused on:
+            <br /> • Streamlined{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            navigation
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            for faster browsing.
+            <br /> •
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            Data-dense layouts
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            to make workspace details readily accessible.
+            <br /> • Interactive
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            {" "}
+            filters to refine searches{" "}
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            and enhance discoverability.
+            <br />{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            • Modern UI components
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            that resonate with Qdesq’s vision for scalability and growth.
+            <br />
+            <br />
+            The{" "}
+          </span>
+          <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+            design process
+          </span>
+          <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+            {" "}
+            progressed from low-fidelity wireframes to high-fidelity prototypes,
+            incorporating user feedback loops to refine functionality and
+            aesthetics, ensuring a cohesive and responsive experience across all
+            devices.
+          </span>
+        </div>
+        <div className="self-stretch justify-start items-start gap-5 inline-flex">
+          <div className="grow shrink basis-0 flex-col justify-start items-start gap-6 inline-flex">
+            <div className="text-white text-base font-semibold font-['SF Pro Display'] leading-7">
+              UI Iterations
+            </div>
+            <div className="self-stretch">
+              <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                For Qdesq, the design process was streamlined due to the
+                adoption of an existing, uniform design system built on
+              </span>
+              <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                {" "}
+                Material UI.
+              </span>
+              <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                {" "}
+                With a strong foundation in place, the focus shifted towards
+                refining layouts and ensuring scalability rather than
+              </span>
+              <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                {" "}
+                creating multiple iterations.{" "}
+              </span>
+              <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                This approach allowed for
+              </span>
+              <span className="text-[#62f186] text-sm font-bold font-['Kalam'] leading-normal">
+                {" "}
+                faster approvals
+              </span>
+              <span className="text-white/80 text-sm font-normal font-['SF Pro Text'] leading-normal">
+                {" "}
+                and a cohesive visual language, aligning seamlessly with Qdesq’s
+                functional and data-driven requirements.
+              </span>
+            </div>
+          </div>
+          <div className="w-[502px] h-[220px] relative">
+            <div className="w-[502px] h-[459.94px] left-0 top-[-240px] absolute">
+              <div className="w-[159.62px] h-[346.04px] left-0 top-[113.72px] absolute">
+                <img src={id1} />
+                <div className="left-[66px] top-[355.70px] absolute text-white/80 text-sm font-['SF Compact'] uppercase leading-tight tracking-wide">
+                  Old
+                </div>
+              </div>
+              <div className="w-[159.62px] h-[347.03px] left-[169.36px] top-[112.91px] absolute">
+                <img src={id2} />
+                <div className="left-[18.64px] top-[355.52px] absolute text-center text-white/80 text-sm font-['SF Compact'] uppercase leading-tight tracking-wide">
+                  Itreation Draft
+                </div>
+              </div>
+              <div className="w-[159.62px] h-[459.79px] left-[342.38px] top-0 absolute">
+                <img src={id3} />
+                <div className="left-[59.62px] top-[468.43px] absolute text-center text-white/80 text-sm font-['SF Compact'] uppercase leading-tight tracking-wide">
+                  Final
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdeateDesign;
