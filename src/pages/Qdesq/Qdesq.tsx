0d53f33f65832344a43495086afb613244fde699
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-01-12 12:43:44 +0530

    Update Qdesq.tsx

--- src/pages/Qdesq/Qdesq.tsx
+++ src/pages/Qdesq/Qdesq.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GradientText from "../../components/GradientText.tsx/GradientText";
+
 import hero from "../../assets/Qdesq/heroqdesq.png";
 import Desc from "../../components/Qdesq/Desc";
 import ProjectInProgress from "../../components/Qdesq/ProjectInProgress";
@@ -10,6 +11,10 @@ import ProblemStatements from "../../components/Qdesq/ProblemStatements";
 import Competitors from "../../components/Qdesq/Competitors";
 import WhatUsersWereThinking from "../../components/Qdesq/WhatUsersWereThinking";
 import MobileDesign from "../../components/Qdesq/MobileDesign";
+import Comp150 from "../../components/Qdesq/Comp150";
+import WebsiteDesigns from "../../components/Qdesq/WebsiteDesigns";
+import IdeateDesign from "../../components/Qdesq/IdeateDesign";
+import Comparison from "../../components/Qdesq/Comparison";
 import ProjectsSection from "../../components/ProjectsSection/ProjectsSection";
 
 const Qdesq: React.FC = () => {
@@ -65,6 +70,12 @@ const Qdesq: React.FC = () => {
         <Competitors />
         <WhatUsersWereThinking />
         <MobileDesign />
+        <div className="w-full h-full flex flex-col justify-center items-center bg-[#e2e8f0]">
+          <Comp150 />
+        </div>
+        <IdeateDesign />
+        <Comparison />
+        <WebsiteDesigns />
       </div>
 
       <div className="text-2xl font-semibold font-['SF Pro Display']">
